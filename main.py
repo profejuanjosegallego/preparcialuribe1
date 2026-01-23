aaff6eb9a42dca4c7aca94757071e47a3e895576
desarrollo <desarrollo@academicos.cesde.edu.co>
2026-01-22 20:21:13 -0500

    feat se crea diccionario

---
+++ main.py
@@ -0,0 +1,19 @@
+contador=0
+'''while contador<5:
+    contador=contador+1
+    print(f"estoy en la vuelta: {contador}")'''
+  
+
+
+
+for _ in range(1,6):
+    
+    diccionario={}
+    diccionario["id"]=int(input("Digita el id del empleado: "))
+    nombres=input("Digita los nombres empleado")
+    documento=input("Digita el documento: ")
+    correo=input("Digita el correo: ")
+    contraseña=input("Digita la contraseña: ")
+
+
+
